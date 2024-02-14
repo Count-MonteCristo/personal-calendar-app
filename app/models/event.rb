5ddbfb36d68752fdf6497a342348f052959acc41
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2024-02-13 18:26:15 -0600

    Updates API to work with React front end

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :date_time, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
   validates :location, presence: true
   validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_09_191113) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_13_212123) do
   create_table "events", force: :cascade do |t|
     t.string "title"
-    t.datetime "date_time"
     t.string "location"
     t.text "description"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
+    t.time "time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
